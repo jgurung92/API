8f5aa3aedf7beea125adc210ae62322b77750d9b
Jiten Gurung <jitengurung@Jitens-MBP.hsd1.pa.comcast.net>
2024-02-19 00:05:34 -0500

    i have corrected the password for the remote user

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
     user: "jgurung",
     host: "dpg-cn1d8hla73kc73ehuu9g-a",
     database: "inventory_6qk5",
-    password: "EoiLRGqXiIhuPd3Nvgvpy9JUFOH",
+    password: "EoiLRGqXiIhuPd3Nvgvpy9JUFOHBugiv",
     // dtabase: nepalSupermarket,
     port: 5432,
 
