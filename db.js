7da1213dab3d18d807863352b3f52b1424219d28
Jiten Gurung <jitengurung@Jitens-MacBook-Pro.local>
2024-02-19 19:09:12 -0500

    I have changed the database file from local to remote

--- db.js
+++ db.js
@@ -1,21 +1,21 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     // This is for connecting database locally:
-    
+    /*
     user: "jitengurung",
     host: "localhost",
     database: "nepalSupermarket",
     port: 5432,
-    
+    */
     // This is for connecting database remotly(in cloud):
-    /*
+   
     user: "jgurung",
     host: "dpg-cn1d8hla73kc73ehuu9g-a",
     database: "inventory_6qk5",
     password: "EoiLRGqXiIhuPd3Nvgvpy9JUFOHBugiv",
     // dtabase: nepalSupermarket,
     port: 5432,
-    */
+   
 });
 
 
